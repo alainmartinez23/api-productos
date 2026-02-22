92d1e2a1dc1f3a9c7faaba23362d37dee38c352f
alainmartinez23 <alainmartinezs23@gmail.com>
2026-02-22 11:31:12 +0100

    Comentada línea debug print URL_DB

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import { initRedis } from "./cache/redisClient.js";
 
 initRedis();
 
-console.log("DATABASE_URL real:", process.env.DATABASE_URL);
+// Línea de DEBUG, ya que me fallaba la conexión a la database
+// console.log("DATABASE_URL real:", process.env.DATABASE_URL);
 
 app.listen(3000, () => {
     console.log("Servidor corriendo en puerto 3000...")
